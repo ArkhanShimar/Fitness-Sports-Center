ecfca9b23d1bc06feaaefebbac1c8c607a1679e0
ArkhanShimar <arkhansimar1@gmail.com>
2026-03-18 15:19:46 +0530

    interactive google map in dark theme

--- src/components/Location.js
+++ src/components/Location.js
@@ -60,28 +60,36 @@ const Location = () => {
               </div>
             </div>
           </div>
-          {/* Right Content - Map */}
+          {/* Right Content - Google Map */}
           <div className="relative">
-            <div className="relative rounded-2xl overflow-hidden border-2 border-primary bg-black h-80 lg:h-96">
-              <div className="absolute inset-0 bg-black">
+            <div className="relative rounded-2xl overflow-hidden border-4 border-primary bg-black h-80 lg:h-96">
+              {/* Google Maps Embed with Dark Theme */}
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.798467128636!2d79.86124731477394!3d6.927078995007317!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae259684e6f0c7b%3A0x8c8b8b8b8b8b8b8b!2sKandy%20Rd%2C%20Colombo%2C%20Sri%20Lanka!5e0!3m2!1sen!2sus!4v1640995200000!5m2!1sen!2sus"
+                width="100%"
+                height="100%"
+                style={{ 
+                  border: 0, 
+                  filter: 'invert(90%) hue-rotate(180deg) saturate(0.8) brightness(0.7)',
+                  WebkitFilter: 'invert(90%) hue-rotate(180deg) saturate(0.8) brightness(0.7)'
+                }}
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+                title="Fitness Sports Center Location"
+              ></iframe>
 
-                {/* Location Pin - Yellow */}
-                <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                  <div className="relative">
-                    {/* Pin */}
-                    <div className="w-8 h-8 bg-primary rounded-full border-2 border-black shadow-lg relative flex items-center justify-center">
-                      <svg className="w-4 h-4 text-black" fill="currentColor" viewBox="0 0 24 24">
-                        <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/>
-                      </svg>
-                    </div>
-                  </div>
-                </div>
+              {/* Dark overlay to enhance dark theme */}
+              <div className="absolute inset-0 bg-black bg-opacity-30 pointer-events-none"></div>
 
-                {/* Navigation arrow - bottom right */}
-                <div className="absolute bottom-4 right-4">
-                    <svg className="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 11l5-5m0 0l5 5m-5-5v12" />
+              {/* Custom Location Pin Overlay */}
+              <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 pointer-events-none">
+                <div className="relative">
+                  <div className="w-8 h-8 bg-primary rounded-full border-2 border-black shadow-lg relative flex items-center justify-center animate-pulse">
+                    <svg className="w-4 h-4 text-black" fill="currentColor" viewBox="0 0 24 24">
+                      <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/>
                     </svg>
+                  </div>
                 </div>
               </div>
             </div>
